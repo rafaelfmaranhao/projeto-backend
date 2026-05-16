6cb47a58bc616df7b202b3f7be56ad11d35785b3
Rafichera <raficho.exe@gmail.com>
2026-05-15 22:54:42 -0300

    adicionado rota cadastro

--- app.py
+++ app.py
@@ -62,10 +62,10 @@ def login():
             }), 400
 
         db = connectDB()
-        cursor = db.cursor(dictionary=True)
+        cursor = db.cursor()
 
         sql = """
-            SELECT id, nome, email, senha_hash, cargo
+            SELECT id, nome, email, senha_hash
             FROM usuarios
             WHERE email = %s;
         """
@@ -83,7 +83,7 @@ def login():
             }), 401
 
         senha_correta = check_password_hash(
-            usuario['senha_hash'],
+            usuario[3],
             senha
         )
 
@@ -92,12 +92,11 @@ def login():
                 'success': False,
                 'message': 'Email ou senha inválidos'
             }), 401
+        
+
 
         token = create_access_token(
-            identity=str(usuario['id']),
-            additional_claims={
-                'cargo': usuario['cargo']
-            }
+            identity=str(usuario[0])
         )
 
         return jsonify({
@@ -107,10 +106,9 @@ def login():
             'token': token,
 
             'usuario': {
-                'id': usuario['id'],
-                'nome': usuario['nome'],
-                'email': usuario['email'],
-                'cargo': usuario['cargo']
+                'id': usuario[0],
+                'nome': usuario[1],
+                'email': usuario[2]
             }
 
         }), 200
@@ -121,10 +119,70 @@ def login():
             'message': str(err)
         }), 500
 
-@app.route('/cadastro')
+@app.route('/cadastro', methods=['POST'])
 def cadastrar():
     try:
         dados = request.get_json()
+
+        if not dados:
+            return jsonify({
+                'success': False,
+                'message': 'JSON inválido'
+            })
+
+        nome = dados.get('nome')
+        email = dados.get('email')
+        senha = dados.get('senha')
+
+        if not nome or not email or not senha:
+            return jsonify({
+                'success': False,
+                'message': 'Email e senha são obrigatórios'
+            }), 400
+        
+        if len(senha) < 6:
+            return jsonify({
+                'success': False,
+                'message': 'Senha deve ter pelo menos 6 caracteres'
+            })
+        
+        db = connectDB()
+        cursor = db.cursor()
+
+        sql = '''
+            SELECT email
+            FROM usuarios
+            WHERE email = %s
+        '''
+        cursor.execute(sql, (email))
+        verifica_email = cursor.fetchone()
+
+        if verifica_email:
+            cursor.close()
+            db.close()
+
+            return jsonify({
+                'success': False,
+                'message': 'Email já cadastrado'
+            }), 409
+        
+        senha_hash = generate_password_hash(senha)
+
+        sql = """
+            INSERT INTO usuarios (nome, email, senha_hash)
+            VALUES (%s, %s, %s)
+        """
+
+        cursor.execute(sql, (nome, email, senha_hash))
+        db.commit()
+
+        cursor.close()
+        db.close()
+
+        return jsonify({
+            'success': True,
+            'message': 'Usuário cadastrado com sucesso.'
+        })
 
     except Exception as err:
         return jsonify({
@@ -134,19 +192,19 @@ def cadastrar():
 
 
 
-@app.route('/registros/cadastrar', methods=['POST'])
-def cad_registro():
+@app.route('/medidores/cadastrar', methods=['POST'])
+def cad_medidor():
     dados = request.get_json()
 
-    registro = dados['registro']
+    medidor = dados['medidor']
     nome = dados['nome_contato']
     telefone = dados['telefone_contato']
 
     sql = '''
-        INSERT INTO registros (registro, nome_contato, telefone_contato)
+        INSERT INTO medidores (medidor, nome_contato, telefone_contato)
         VALUES (%s, %s, %s)
     '''
-    executeSQL(sql, (registro, nome, telefone))
+    executeSQL(sql, (medidor, nome, telefone))
 
     return jsonify({
         'success': True,
@@ -154,26 +212,26 @@ def cad_registro():
     })
 
 
-@app.route('/registros', methods=['GET'])
-def pesq_registro():
+@app.route('/medidores', methods=['GET'])
+def pesq_medidor():
     pesquisa = request.args.get('q', '')
 
     sql = '''
-        SELECT * FROM registros
+        SELECT * FROM medidores
         WHERE CAST(id AS TEXT) = %s
-        OR registro ILIKE %s
+        OR medidor ILIKE %s
     '''
     resultado = executeSQL(sql, (pesquisa, f'%{pesquisa}%'), fetch=True)
 
     return ''
 
-@app.route('/registros/<int:id>')
-def pag_registro(id):
+@app.route('/medidores/<int:id>')
+def pag_medidor(id):
     return ''
 
 
-@app.route('/registros/atualizar', methods=['PUT'])
-def att_registro():
+@app.route('/medidores/atualizar', methods=['PUT'])
+def att_medidor():
     dados = request.get_json()
 
     id = dados['id']
@@ -181,7 +239,7 @@ def att_registro():
     telefone = dados['telefone']
 
     sql = '''
-        UPDATE registros
+        UPDATE medidores
         SET nome = %s,
             telefone = %s
         WHERE id = %s
@@ -191,21 +249,21 @@ def att_registro():
     return ''
 
 
-@app.route('/registros/deletar', methods=['DELETE'])
-def del_registro():
+@app.route('/medidores/deletar', methods=['DELETE'])
+def del_medidor():
     dados = request.get_json()
 
     id = dados['id']
 
-    sql = 'DELETE FROM registros WHERE id = %s'
+    sql = 'DELETE FROM medidores WHERE id = %s'
     executeSQL(sql, (id))
 
     return ''
 
 
 # Leituras
-@app.route('/leituras/<int:registro_id>')
-def cad_leitura(registro_id):
+@app.route('/leituras/<int:medidor_id>')
+def cad_leitura(medidor_id):
     dados = request.get_json()
 
     leitura = dados['leitura']
@@ -213,10 +271,10 @@ def cad_leitura(registro_id):
     tipo = dados['tipo']
 
     sql = '''
-        INSERT INTO leituras (leitura, data_leitura, fk_registro_id)
+        INSERT INTO leituras (leitura, data_leitura, fk_medidor_id)
         VALUES (%s, %s, %s)
     '''
-    executeSQL(sql, (leitura, data_leitura, registro_id))
+    executeSQL(sql, (leitura, data_leitura, medidor_id))
 
     return ''
 
@@ -230,7 +288,7 @@ def att_leitura():
     telefone = dados['telefone']
 
     sql = '''
-        UPDATE registros
+        UPDATE medidores
         SET nome = %s,
             telefone = %s
         WHERE id = %s
@@ -240,13 +298,13 @@ def att_leitura():
     return ''
 
 
-@app.route('/registros/deletar', methods=['DELETE'])
-def del_registro():
+@app.route('/leituras/deletar', methods=['DELETE'])
+def del_leituras():
     dados = request.get_json()
 
     id = dados['id']
 
-    sql = 'DELETE FROM registros WHERE id = %s'
+    sql = 'DELETE FROM medidores WHERE id = %s'
     executeSQL(sql, (id))
 
     return ''
